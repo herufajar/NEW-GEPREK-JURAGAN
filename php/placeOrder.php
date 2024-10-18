43ada2fb286e13a949065d8ff764fba7f5102f27
herufajar <herufajarjulianto@gmail.com>
2024-10-18 21:58:00 +0700

    Perbaiki penanganan CORS di placeOrder.php

--- php/placeOrder.php
+++ php/placeOrder.php
@@ -1,7 +1,13 @@
 <?php 
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
+
+// Tangani permintaan OPTIONS
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 // Ambil data dari request
 $data = json_decode(file_get_contents("php://input"), true);
